3a520513bd153e0bae8bae7d6b434f94a903aa2b
Josue Martinez <martinez344j@gmail.com>
2023-08-08 21:20:47 -0400

    removed un-needed files

--- variable_functions.h
+++
@@ -1,5 +0,0 @@
-void readAppDataFromEEPROM(char* ssid, char* password, bool (&waterTimer)[], bool (&lightTimer)[]);
-void saveAppDataToEEPROM(const char* ssid, const char* password, bool (&waterTimer)[], bool (&lightTimer)[]);
-void saveArraysToEEPROM(int index, bool waterTimer[], bool lightTimer[]);
-void readArraysFromEEPROM(bool waterTimer[], bool lightTimer[]);
-void clearEEPROM(int size);
